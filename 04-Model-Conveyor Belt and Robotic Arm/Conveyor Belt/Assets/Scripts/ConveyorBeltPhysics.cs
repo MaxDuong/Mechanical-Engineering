b61d974bf9da217fa624876153ceade908e3140f
Max Duong <duonghoanvu1@gmail.com>
2024-12-22 16:13:47 -0600

    Add Conveyor Speed

--- 04-Model-Conveyor Belt and Robotic Arm/Conveyor Belt/Assets/Scripts/ConveyorBeltPhysics.cs
+++ 04-Model-Conveyor Belt and Robotic Arm/Conveyor Belt/Assets/Scripts/ConveyorBeltPhysics.cs
@@ -1,20 +1,23 @@
 using UnityEngine;
 
-public class ConveyorBeltPhysics : MonoBehaviour
-{
+public class ConveyorBeltPhysics : MonoBehaviour {
     public float speed = 0.1f; // Speed of the conveyor belt
+    public StartButtonControl script; // Reference to the StartButton script
+    public ConveyorSpeedControl SpeedControl;
     Rigidbody conv; // Rigidbody component of the Conveyor Belt
+    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
+    void Start() {
         conv = GetComponent<Rigidbody>(); // Get the Rigidbody component attached to the Conveyor Belt
     }
 
     // FixedUpdate is called once per frame
-    void FixedUpdate()
-    {
-        Vector3 pos = conv.position; // Get the position of the Conveyor Belt
-        conv.position = conv.position + Vector3.right * speed * Time.deltaTime; // Move the Conveyor Belt to the left
-        conv.MovePosition(pos); // Update the position of the Conveyor Belt
+    void FixedUpdate() {
+        if(script.start){
+            Vector3 pos = conv.position; // Get the position of the Conveyor Belt
+            conv.position = conv.position + Vector3.right * SpeedControl.ConveyorSpeed * Time.deltaTime; // Move the Conveyor Belt to the left
+            conv.MovePosition(pos); // Update the position of the Conveyor Belt
+        }
+        
     }
 }
--- 04-Model-Conveyor Belt and Robotic Arm/Conveyor Belt/Assets/Scripts/ConveyorSpeedControl.cs
+++ 04-Model-Conveyor Belt and Robotic Arm/Conveyor Belt/Assets/Scripts/ConveyorSpeedControl.cs
@@ -3,14 +3,16 @@ using UnityEngine;
 public class ConveyorSpeedControl : MonoBehaviour {
     public float ConveyorPulleysSpeed = 50.0f;
     public float BeltSpeed;
-
+    public float ConveyorSpeed; 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start() {
         BeltSpeed = ConveyorPulleysSpeed / 100 * -1;
+        ConveyorSpeed = ConveyorPulleysSpeed / 100 * 1.2f;
     }
 
     // Update is called once per frame
     void Update() {
         BeltSpeed = ConveyorPulleysSpeed / 100 * -1;
+        ConveyorSpeed = ConveyorPulleysSpeed / 100 * 1.2f;
     }
 }
